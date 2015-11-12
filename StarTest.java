cc800861dda246ba0c82475da856f5b5d8498842
unknown <hoanggnguyen@L-SIN-00441096.paypalcorp.com>
2015-11-12 18:37:46 +0800

    error on dimond shape with square inside

--- StarTest.java
+++ StarTest.java
@@ -1,8 +1,21 @@
 public class StarTest {
 	public static void main(String[] args) {
-		int M = 7;
-		int N = M * 2;
-		
+		printStar(2);
+		printStar(3);
+		printStar(4);
+
+		printStarRec(3);
+		printStarRec(4);
+		printStarRec(5);
+	}
+
+	/*
+	Print the dimond shape with X
+	*/
+	private static void printStar(int mid) {
+		int N = mid * 2; // dimond shape need to have odd number of rows (rows = colums)
+		// thus rows = N + 1 (2k+1 is odd)
+
 		for (int j = 0; j <= N;j++) {
 			for (int i = 0; i <= N; i++) {
 				if (j <= N/2) { // 1st half
@@ -23,6 +36,41 @@ public class StarTest {
 							System.out.print("+");
 						else
 							System.out.print("*");
+				}
+			}				
+			System.out.println();
+		}
+	}
+
+	private static void printStarRec(int mid) {
+		int N = mid * 2; // dimond shape need to have odd number of rows (rows = colums)
+		// thus rows = N + 1 (2k+1 is odd)
+		
+		int k = N/2;
+		if (N%2 != 0)
+			k++;
+
+		for (int j = 0; j <= N;j++) {
+			for (int i = 0; i <= N; i++) {
+				if (j <= N/2) { // 1st half
+					if (i < N/2 - j)
+						System.out.print(" ");
+					else if (i <= N/2 + j) {
+						if((i > (N-k)/2 && i <= (N+k)/2) && (j > (N-k)/2 && j <= (N+k)/2))
+							System.out.print("+");
+						else
+							System.out.print("*");
+					}
+				}
+				else { // 2nd half
+					if (i < j - N/2)
+						System.out.print(" ");
+					else if (i <= N-(j-N/2)) {
+						if((i > (N-k)/2 && i <= (N+k)/2) && (j > (N-k)/2 && j <= (N+k)/2))
+							System.out.print("+");
+						else
+							System.out.print("*");
+					}
 				}
 			}				
 			System.out.println();
